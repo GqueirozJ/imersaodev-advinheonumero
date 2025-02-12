15ec33af1d19865af75b3355910077ea327c6e8f
GqueirozJ <112795244+GqueirozJ@users.noreply.github.com>
2025-02-12 18:12:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+var contador = 0;
+
+function Chutar() {
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Está certo! Parabens :)";
+  } else if (document.getElementById("valor").value == "") {
+    alert("Por favor, coloque um numero de 0 a 10");
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Coloque um número de 0 a 10";
+  } else {
+    elementoResultado.innerHTML = "Você errou! :(";
+  }
+}
